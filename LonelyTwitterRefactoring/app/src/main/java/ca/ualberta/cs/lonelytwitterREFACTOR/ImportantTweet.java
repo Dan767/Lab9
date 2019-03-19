d70df332324bb13a4b6d16787c544a170d50933f
Dan767 <37282597+Dan767@users.noreply.github.com>
2019-03-19 17:46:40 -0600

    Add files via upload

---
+++ LonelyTwitterRefactoring/app/src/main/java/ca/ualberta/cs/lonelytwitterREFACTOR/ImportantTweet.java
@@ -0,0 +1,62 @@
+package ca.ualberta.cs.lonelytwitterREFACTOR;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class ImportantTweet extends NormalLonelyTweet {
+
+    private static final long serialVersionUID = 1L;
+    protected Date tweetDate;
+    protected String tweetBody;
+
+
+
+    public ImportantTweet() {
+    }
+
+    public ImportantTweet(String text, Date date) {
+        this.tweetDate = date;
+        this.tweetBody = text;
+    }
+
+    public Date getTweetDate() {
+        return tweetDate;
+    }
+
+    public void setTweetDate(Date tweetDate) {
+        this.tweetDate = tweetDate;
+    }
+
+    public String getTweetBody() {
+        return tweetBody;
+    }
+
+    public void setTweetBody(String tweetBody) {
+        this.tweetBody = tweetBody;
+    }
+
+    private void writeObject(java.io.ObjectOutputStream out) throws IOException {
+        out.writeObject(tweetDate);
+        out.writeObject(tweetBody);
+    }
+
+    private void readObject(java.io.ObjectInputStream in) throws IOException,
+            ClassNotFoundException {
+        tweetDate = (Date) in.readObject();
+        tweetBody = (String) in.readObject();
+    }
+
+    public boolean isValid() {
+        if (tweetBody.trim().length() == 0
+                || tweetBody.trim().length() > 10) {
+            return false;
+        }
+
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return getTweetDate() + " | " + getTweetBody() ;
+    }
+}
